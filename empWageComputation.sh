bdbbfb212c9625ee60268b95c824aa0a98c6a3a3
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-22 21:08:17 +0530

    Modified: Logic to store day and daily wage along with total wage of employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -47,12 +47,15 @@ empDailyWage[$totalWorkingDays]="$( calculateDailyWage $workHours )"
 dayWiseWages[$totalWorkingDays]="$(calculateDailyWage $workHours)"
 
 done
-
 totalSalary=$(( $totalWorkHours * $EMP_RATE_PER_HR ));
+echo -e "\n::::::::::::Printing Days from array::::::"
+echo ${!empDailyWage[@]}
 
 echo -e "\n::::::::::::Printing salary stored day wise in an array:::::::::::::::"
 echo ${empDailyWage[@]}
 
+echo -e "\n::::::::::::Printing Days from dictionary::::::"
+echo ${!dayWiseWages[@]}
 
 echo -e "\n::::::::::::Printing salary stored day wise in dictionary:::::::::::::::"
 echo ${dayWiseWages[@]}
