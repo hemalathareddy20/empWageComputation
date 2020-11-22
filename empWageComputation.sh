4214f482b3e0d45a0f587855ba7010bd848ddccc
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-22 19:44:47 +0530

    Added: Logic to check Employee Attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 echo "Welcome to Employee wage Computation program on Master Branch"
+isPresent=1
+random=$(( RANDOM%2 ))
+echo "Random number: "$random
+if(( $random == $isPresent ))
+then
+echo "Employee is Present!"
+else
+echo "Employee is absent!"
+fi
+
