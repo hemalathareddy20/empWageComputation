9bff0879647b29c53d804f2d0852d7e1c0e88ff3
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-22 19:50:06 +0530

    Modified: Logic to check Employee Daily Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,17 @@
-#!/bin/bash
-echo "Welcome to Employee wage Computation program on Master Branch"
+#! /bin/bash
+echo "Welcome to Employee Wage Computation Program on Master Branch"
 isPresent=1
 random=$(( RANDOM%2 ))
 echo "Random number: "$random
 if(( $random == $isPresent ))
 then
 echo "Employee is Present!"
+empHrs=8
+ratePerHour=20
+salary=$(( $empHrs * $ratePerHour))
 else
 echo "Employee is absent!"
+salary=0
 fi
 
+echo "Salary: $salary"
